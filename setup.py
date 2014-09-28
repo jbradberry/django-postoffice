8fdc43299138f56d15c3d7da6ed4f95c40ea60f8
Jeff Bradberry <jeff.bradberry@gmail.com>
2014-09-27 23:29:55 -0400

    Added tox.

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 from distutils.core import setup
 import os
 
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir:
@@ -19,21 +29,22 @@ for dirpath, dirnames, filenames in os.walk('postoffice'):
         for f in filenames:
             data_files.append(os.path.join(prefix, f))
 
-setup(name='django-postoffice',
-      description='',
-      version="0.1.0dev",
-      author='Jeff Bradberry',
-      author_email='jeff.bradberry@gmail.com',
-      url='http://github.com/jbradberry/django-postoffice',
-      package_dir={'postoffice': 'postoffice'},
-      packages=packages,
-      package_data={'postoffice': data_files},
-      classifiers=['Development Status :: 2 - Pre-Alpha',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python'],
-      long_description=open('README.rst').read(),
-      )
+setup(
+    name='django-postoffice',
+    description='',
+    version="0.1.0dev",
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    url='http://github.com/jbradberry/django-postoffice',
+    package_dir={'postoffice': 'postoffice'},
+    packages=packages,
+    package_data={'postoffice': data_files},
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python'],
+    long_description=read_file('README.rst'),
+)
